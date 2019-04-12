15caaba82c83070fbfd2cc60cab4f9ca627aa30d
ills5771 <ilyayelizarov1@gmail.com>
2019-04-12 10:53:19 -0700

    adds test checking if status code is 422 when adding game without providing genre and empty object

--- games/gamesModel.spec.js
+++ games/gamesModel.spec.js
@@ -19,8 +19,21 @@ describe("gamesModel.js", () => {
           expect(res.status).toBe(200);
         });
     });
+    it("should return an array", async () => {
+      return request(server)
+        .get("/games")
+        .then(res => {
+          expect(res.text).toEqual([]);
+        });
+      //   const response = await request(server).get("/games");
+
+      //   expect(response.text).toBe([]);
+    });
   });
   describe("POST Route", () => {
+    beforeEach(async () => {
+      await db("games").truncate();
+    });
     it("should return status code 201 for POST", async () => {
       const expectedBody = { title: "test1", genre: "test-genre1" };
       const response = await request(server)
@@ -29,8 +42,24 @@ describe("gamesModel.js", () => {
 
       expect(response.status).toEqual(201);
     });
-    it("should return status code 422 for POST", async () => {
+    it("should return status code 422 for POST(--if genre not provided)", async () => {
       const expectedBody = { title: "test1" };
+      const response = await request(server)
+        .post("/games")
+        .send(expectedBody);
+
+      expect(response.status).toEqual(422);
+    });
+    it("should return status code 422 for POST(--if title not provided)", async () => {
+      const expectedBody = { genre: "test1-genre" };
+      const response = await request(server)
+        .post("/games")
+        .send(expectedBody);
+
+      expect(response.status).toEqual(422);
+    });
+    it("should return status code 422 for POST(--if genre and title not provided)", async () => {
+      const expectedBody = {};
       const response = await request(server)
         .post("/games")
         .send(expectedBody);
