ed6f41acdfa5f1e6d168000db4e7886498bfc29c
ills5771 <ilyayelizarov1@gmail.com>
2019-04-12 08:22:46 -0700

    creates games_table with knex migrations

---
+++ data/migrations/20190412082024_games_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("games", tbl => {
+    tbl.increments();
+
+    tbl.string("title").notNullable();
+    tbl.string("genre").notNullable();
+    tbl.integer("year");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("games");
+};
